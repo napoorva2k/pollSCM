0afd6a60328da008676ac8afb0d93edd1afc3a6a
root <root@ip-172-31-45-170.ap-south-1.compute.internal>
2024-02-12 12:56:09 +0000

    added new txt file

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ agent any
     stages {
         stage('SCM') {
             steps {
-                 sh "git ls-remote -h -- git@github.com:napoorva2k/pollSCM.git HEAD"
+                 echo "helloo...!!!"
             }
         }
     }
